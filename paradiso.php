8cd4b593609443479d0f07d2f42ff6542f6a7cc6
Aarav <asaki1@icloud.com>
2021-12-04 18:10:02 -0800

    converted to php, extended navbar template

---
+++ paradiso.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link type="text/css" rel="stylesheet" href="css/template.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Paradiso</title>
+  </head>
+  <body>
+  <?php include_once 'template.php';?>
+    <h2>Paradiso</h2>
+    <script src="js/index.js"></script>
+  </body>
+</html>
---
+++ template.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="index.php"
+                ><img
+                  src="https://toppng.com/uploads/preview/broadway-clipart-drama-faces-drama-logo-11562970981wsznppujg6.png"
+                  width="50"
+                  height="50"
+                />
+              </a>
+            </li>
+
+            <li style="margin-top: 15px" class="nav-item">
+              <a class="nav-link active" href="inferno.php">Inferno</a>
+            </li>
+
+            <li style="margin-top: 15px" class="nav-item">
+              <a class="nav-link active" href="purgatorio.php">Purgatorio</a>
+            </li>
+
+            <li style="margin-top: 15px" class="nav-item">
+              <a class="nav-link active" href="paradiso.php">Paradiso</a>
+            </li>
+
+            <li style="margin-top: 15px" class="nav-item">
+              <a class="nav-link active" href="store.php">Merchandise</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h1>The Divine Comedy</h1>
