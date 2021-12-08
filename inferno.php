d2effa5db903cc30796f40b9affc73e8e0374f67
Justin Shen <jjshen81205@gmail.com>
2021-12-07 20:53:47 -0800

    Added Inferno content + styling

--- inferno.php
+++ inferno.php
@@ -15,7 +15,63 @@
   </head>
   <body>
   <?php include_once 'template.php';?>
-    <h2>Inferno</h2>
+    <h1>Inferno</h1>
+    <div style="text-align: center;">
+    <img src = "https://media.discordapp.net/attachments/700479408263397416/918000257755938827/-VBWvHn1Skz8LVzmcrTD4w2Fhellthumb800.png"
+    >
+</div>
     <script src="js/index.js"></script>
+
+    <div >
+      <p>
+        The first stop is Inferno. Here souls are punished for all of eternity based on their sins committed in life. The 9 Circles of Hell are separated based on the severity of sin and brutality of punishment. 
+      </p>
+        <h2> Limbo </h2>
+
+      <p class="one">
+        The first circle is one of mercy. Here lie the unbaptized and non-christian who have not sinned. Those that reside within this circle are not tormented and only grieve that they must live eternally without God’s Grace.
+      </p>
+        <h2> Lust </h2>
+      <p class="one">
+        The second circle subsumes those overcome by lust. They face fierce winds that throw them throughout the land. The winds prevent them from experiencing rest and peace. People of note include: Cleopatra, Achilles, and Helen of Troy.
+
+      </p>
+        <h2> Gluttony </h2>
+      <p class="one">
+        The third circle houses the most gluttonous of sinners. Cerberus is also found here acting as a warden. The punishment involves being rained on for all of eternity while laying in waste.
+      </p>
+        <h2> Greed </h2>
+      <p class="one">
+        The fourth circle is guarded by Plutus, Greek god of wealth, hence it being the destination for those who are too greedy. People in this circle are forced to roll around large rocks whose size is based on their greed. The souls are then forced to roll their rocks into each other as a form of jousting.
+
+      </p>
+      <h2> Anger </h2>
+        <p class="one">
+          The fifth circle is represented by the River Styx. Those who were too angry in their lives are forced to permanently drown and fight in the river. Many florentine politicians are found here
+
+          <p>
+      <h2> Heresy </h2>
+        <p class="one">
+          The sixth circle is home to all the heretics who are forced into burning coffins. People like Greek philosopher Epicurus and Holy Roman Emperor Frederick II can be found there.
+
+          </p>
+
+      <h2> Violence </h2>
+
+        <p class="one">The seventh circle is the broadest circle as it houses those who committed acts of violence. It is guarded by the minotaur and inhabited by the Centaurs. Violent conquerors of the past are forced into a lake of boiling blood and if any find their way out, they are pursued and shot by the centaurs to be returned to the lake. People who commit violence on themselves end up as trees who are eternally picked on by harpies. Those who commit violence against nature and the church are doomed to experience rain made of fire.
+          
+        </p>
+      <h2> Fraud</h2>
+      <p class="one">
+        The eighth circle holds those that have committed some form of dishonest crime during their life. Thieves, Hypocrites, and Corrupt Politicians are all submitted to a pit of snakes while simultaneously being permanently on fire. Alchemists, who deceive people through their scam based potions, are inflicted with every possible disease at the same time. 
+        </p>
+      <h2> Treachery</h2>
+      <p class="one">
+        The ninth and final circle houses Lucifer and three other souls. Lucifer is depicted with three heads and is suspended in a huge lake of ice. Each of the three heads are chewing on the most treacherous souls in history (According to Dante). Judas, the man who revealed Jesus to a group of people who had come arrest him, Marcus, one of the two men who led the assassination of Julius ceasar, and Galius, the other man who led that event.
+        </p>
+
+    </div>
+      
+
   </body>
 </html>
