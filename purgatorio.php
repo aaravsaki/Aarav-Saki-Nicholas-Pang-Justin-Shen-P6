a29cd1dbf794c673bc8c0dbcb6f5e3f7b1eddda2
10033333 <10033333@american.fmtusd.org>
2021-12-08 13:50:26 -0800

    Purgatorio

--- purgatorio.php
+++ purgatorio.php
@@ -7,7 +7,7 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <link type="text/css" rel="stylesheet" href="css/template.css" />
+    <link type="text/css" rel="stylesheet" href="css/purgatorio.css" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -15,7 +15,71 @@
   </head>
   <body>
   <?php include_once 'template.php';?>
-    <h2>Purgatorio</h2>
+    <h1>Purgatorio</h1>
+    <div style="text-align: center;">
+    <img src = "https://media.discordapp.net/attachments/700479408263397416/918000257755938827/-VBWvHn1Skz8LVzmcrTD4w2Fhellthumb800.png"
+    >
+</div>
     <script src="js/index.js"></script>
+
+    <div >
+      <p>
+      Purgatory is a place for souls who weren’t good enough to go to heaven but not horrible enough to go to Hell. It consists of seven terraces corresponding to the seven deadly sins and two extra terraces. One must climb up to cleanse themselves of all sins in order to enter heaven.
+
+      </p>
+        <h2> Ante-Purgatory </h2>
+
+      <p class="one">
+      Before the climb begins, people must wait the equivalent of their lifetime in order to start the climb. At the gate, each soul has the letter “P” carved into their head seven times. The letters are to be erased the moment they pass each terrace.
+
+      </p>
+        <h2> Pride </h2>
+      <p class="one">
+      Souls here practice the virtue of humility, opposite of pride. They are forced to carry huge stones symbolizing their pride while climbing. The souls carry stones in order to force them to bow.
+
+
+      </p>
+        <h2> Envy </h2>
+      <p class="one">
+      Souls in this terrace practice generosity, opposite of envy. They have their eyes sewn shut while disembodied voices guide them up while talking about the great benefits of generosity.
+
+      </p>
+        <h2> Wrath </h2>
+      <p class="one">
+      Souls here practice the virtue of peace, opposite of wrath. This terrace is filled with an acrid mist that souls must path through to arrive at the next terrace.
+
+
+      </p>
+      <h2> Sloth </h2>
+        <p class="one">
+        Souls here practice the virtue of fortitude, opposite of sloth. Souls here just participate in strenuous exercise until they reach the next terrace.
+
+</p>
+      <h2> Greed </h2>
+        <p class="one">
+          Souls here practice the value of justice, opposite of greed. Souls are stuck faced down at the floor and must wait for earthquakes that occur every time a soul goes to heaven in order to move them.
+
+
+          </p>
+
+      <h2> Gluttony </h2>
+
+        <p class="one">Souls here practice the value of temperance, opposite of gluttony. Here souls climb as they are surrounded by food that is forever out of reach.
+
+          
+        </p>
+      <h2> Lust</h2>
+      <p class="one">
+      Souls here practice the value of Purity, opposite of lust. The souls must pass a wall of fire before they see the Earthly Paradise.
+
+        </p>
+      <h2> Earthly Paradise</h2>
+      <p class="one">
+      This is the very top of purgatory and represents the Garden of Eden. 
+        </p>
+
+    </div>
+      
+
   </body>
 </html>
