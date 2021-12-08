1488143dacbb4b6347b5e449777fc6ee3268c24c
10033333 <10033333@american.fmtusd.org>
2021-12-08 14:01:35 -0800

    Added Paradiso

--- paradiso.php
+++ paradiso.php
@@ -7,7 +7,7 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <link type="text/css" rel="stylesheet" href="css/template.css" />
+    <link type="text/css" rel="stylesheet" href="css/paradiso.css" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -15,7 +15,75 @@
   </head>
   <body>
   <?php include_once 'template.php';?>
-    <h2>Paradiso</h2>
+    <h1>Paradiso</h1>
+    <div style="text-align: center;">
+    <img src = "https://media.discordapp.net/attachments/700479408263397416/918000257755938827/-VBWvHn1Skz8LVzmcrTD4w2Fhellthumb800.png"
+    >
+</div>
     <script src="js/index.js"></script>
+
+    <div >
+      <p>
+      Paradiso is the equivalent of heaven but it references space. It is also a representation of the old geocentric model of space. Each sphere of heaven represents a planet It is described as blindingly bright as God sheds light onto heaven.
+
+      </p>
+        <h2> Moon </h2>
+
+      <p class="one">
+      The souls who dwell here practice the belief on the inconstant. The souls who were always in constant motion get to live on the moon. People who gave up their vows for a good cause end up here.
+
+      </p>
+        <h2> Mercury </h2>
+      <p class="one">
+      Mercury, the second sphere, is the closest planet to the sun so the souls that were ambitious in their life get to live on Mercury. The sun also represents God, as the light of the ambitious pales in comparison to the glory of god just as Mercury pales in size compared to the sun.
+
+      </p>
+        <h2> Venus </h2>
+      <p class="one">
+      The third sphere, Venus, is home to lovers as Venus is the god of love in the roman mythos.
+
+      </p>
+        <h2> The Sun </h2>
+      <p class="one">
+      The fourth sphere is the sun which does not make a lot of sense but just roll with it. The sun is home to the wise and holds twelve souls of people who helped intellectually illuminate the world. 
+
+
+      </p>
+      <h2> Mars </h2>
+        <p class="one">
+        The fifth sphere is Mars and houses “Warriors of Faith” or people who died in battle in the name of god. It contains these people because Mars in Roman mythology is the god of war.
+
+
+          <p>
+      <h2> Jupiter </h2>
+        <p class="one">
+        The sixth sphere represents Jupiter and is home to the greatest rulers of the past. Monarchs like Constantine reside here.
+
+          </p>
+
+      <h2> Saturn </h2>
+
+        <p class="one">The seventh sphere represents Saturn and is home to Contemplatives or advanced thinkers. 
+
+          
+        </p>
+      <h2> The Fixed Stars</h2>
+      <p class="one">
+      Fixed stars was the name for all the stars that seemed to not move and served as the outermost layer of the universe in a geocentric model. Various important christian individuals reside here like Saint Peter, The Virgin Mary, and Adam. The people here are the holiest of all people except for Jesus
+
+        </p>
+      <h2> The Primum Mobile </h2>
+      <p class="one">
+      The Primum Mobile is the last physical sphere of Heaven and realm of the Angels. Within this sphere lies nine other circles representing the Angelic Hierarchy
+
+        </p>
+      <h2> The Empyrean </h2>
+      <p class="one"> 
+      This is the holiest of all places and a realm of pure holy light. Here souls can see the face of god and are the closest to god anyone could possibly get.
+</p>
+
+    </div>
+      
+
   </body>
 </html>
