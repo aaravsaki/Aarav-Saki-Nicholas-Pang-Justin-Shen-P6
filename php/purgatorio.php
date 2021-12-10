36263e4ace22c41cb3d024fa2160f4433b81efce
Aarav Saki <asaki1@icloud.com>
2021-12-10 15:11:08 -0800

    fixed pathing issue

--- php/purgatorio.php
+++ php/purgatorio.php
@@ -7,7 +7,7 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <link type="text/css" rel="stylesheet" href="purgatorio.css" />
+    <link type="text/css" rel="stylesheet" href="../css/purgatorio.css" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
