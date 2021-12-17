de0fc08c06157a0e072d94266475516d0cb0ede1
10033333 <10033333@american.fmtusd.org>
2021-12-17 14:54:00 -0800

    added pictures for all body pages

--- php/paradiso.php
+++ php/paradiso.php
@@ -7,7 +7,7 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <link type="text/css" rel="stylesheet" href="../css/paradiso.css" />
+    <link type="text/css" rel="stylesheet" href="../css/paradisobg.css" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -44,60 +44,80 @@
     <div >
      
         <h2 id="one"> Moon </h2>
+        <img src="../Merch Images/Moon.jpg"
+          class="image1">
 
       <p class="two">
       The souls who dwell here practice the belief on the inconstant. The souls who were always in constant motion get to live on the moon. People who gave up their vows for a good cause end up here.
 
       </p>
         <h2 id="two"> Mercury </h2>
+        <img src="../Merch Images/Mercury.jpg"
+          class="image2">
       <p class="one">
       Mercury, the second sphere, is the closest planet to the sun so the souls that were ambitious in their life get to live on Mercury. The sun also represents God, as the light of the ambitious pales in comparison to the glory of god just as Mercury pales in size compared to the sun.
 
       </p>
         <h2 id="three"> Venus </h2>
+        <img src="../Merch Images/Venus.jpg"
+          class="image1">
       <p class="two">
       The third sphere, Venus, is home to lovers as Venus is the god of love in the roman mythos.
 
       </p>
         <h2 id="four"> The Sun </h2>
+        <img src="../Merch Images/The SUn.jpg"
+          class="image2">
       <p class="one">
       The fourth sphere is the sun which does not make a lot of sense but just roll with it. The sun is home to the wise and holds twelve souls of people who helped intellectually illuminate the world. 
 
 
       </p>
       <h2 id="five"> Mars </h2>
+      <img src="../Merch Images/Mars.jpg"
+          class="image1">
         <p class="two">
         The fifth sphere is Mars and houses “Warriors of Faith” or people who died in battle in the name of god. It contains these people because Mars in Roman mythology is the god of war.
 
 
           <p>
       <h2 id="six"> Jupiter </h2>
+      <img src="../Merch Images/Jupiter.jpg"
+          class="image2">
         <p class="one">
         The sixth sphere represents Jupiter and is home to the greatest rulers of the past. Monarchs like Constantine reside here.
 
           </p>
 
       <h2 id="seven"> Saturn </h2>
+      <img src="../Merch Images/Saturn.jpg"
+          class="image1">
 
         <p class="two">The seventh sphere represents Saturn and is home to Contemplatives or advanced thinkers. 
 
           
         </p>
       <h2 id="eight"> The Fixed Stars</h2>
+      <img src="../Merch Images/fixed stars.jpg"
+          class="image2">
       <p class="one">
       Fixed stars was the name for all the stars that seemed to not move and served as the outermost layer of the universe in a geocentric model. Various important christian individuals reside here like Saint Peter, The Virgin Mary, and Adam. The people here are the holiest of all people except for Jesus
 
         </p>
       <h2 id="nine"> The Primum Mobile </h2>
+      <img src="../Merch Images/primummobile.jpg"
+          class="image1">
       <p class="two">
       The Primum Mobile is the last physical sphere of Heaven and realm of the Angels. Within this sphere lies nine other circles representing the Angelic Hierarchy
 
         </p>
       <h2 id="ten"> The Empyrean </h2>
+      <img src="../Merch Images/god2.jpg"
+          class="image2">
       <p class="one"> 
       This is the holiest of all places and a realm of pure holy light. Here souls can see the face of god and are the closest to god anyone could possibly get.
 </p>
-
+<h3 style="opacity: 0"> . </h3>
     </div>
       
 
